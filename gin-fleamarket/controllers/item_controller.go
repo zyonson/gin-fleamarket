f62ca7b0648eecaec0629442f58b29b56677ffbf
zyonson <wakeup5216@gmail.com>
2025-06-07 13:05:18 +0900

    git add controller

---
+++ gin-fleamarket/controllers/item_controller.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"gin-fleamarket/services"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type IItemController interface {
+	FindAll(ctx *gin.Context)
+}
+
+type ItemController struct {
+	service services.IItemService
+}
+
+func NewItemController(service services.IItemService) IItemController {
+	return &ItemController{service: service}
+}
+
+func (c *ItemController) FindAll(ctx *gin.Context) {
+	items, err := c.service.FindAll()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data": items})
+}
