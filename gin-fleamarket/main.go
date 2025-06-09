1af227ed36114018029fd3d19dd06157171b0031
zyonson <wakeup5216@gmail.com>
2025-06-10 01:09:28 +0900

    cors設定

--- gin-fleamarket/main.go
+++ gin-fleamarket/main.go
@@ -9,6 +9,7 @@ import (
 	"gin-fleamarket/repositories"
 	"gin-fleamarket/services"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -31,6 +32,7 @@ func main() {
 	authController := controllers.NewAuthController(authService)
 
 	r := gin.Default()
+	r.Use(cors.Default())
 	itemRouter := r.Group("/items")
 	itemRouterWithAuth := r.Group("/items", middlewares.AuthMiddleware(authService))
 	authRouter := r.Group("/auth")
