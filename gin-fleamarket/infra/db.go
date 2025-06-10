a4bf49345bd8f3594907b2b7f79ba1f7fb62b327
zyonson <wakeup5216@gmail.com>
2025-06-10 22:06:05 +0900

    FindAllメソッドテスト追加

--- gin-fleamarket/infra/db.go
+++ gin-fleamarket/infra/db.go
@@ -2,13 +2,16 @@ package infra
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func SetupDB() *gorm.DB {
+	env := os.Getenv("ENV")
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo",
 		os.Getenv("DB_HOST"),
@@ -18,7 +21,18 @@ func SetupDB() *gorm.DB {
 		os.Getenv("DB_PORT"),
 	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	var (
+		db  *gorm.DB
+		err error
+	)
+
+	if env == "prod" {
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		log.Println("Setup postgresql database")
+	} else {
+		db, err = gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+		log.Println("Setup sqlite database")
+	}
 	if err != nil {
 		panic("Failed to connect database")
 	}
---
+++ gin-fleamarket/main_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"gin-fleamarket/infra"
+	"gin-fleamarket/models"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+)
+
+func TestMain(m *testing.M) {
+	if err := godotenv.Load(".env.test"); err != nil {
+		log.Fatalln("Error loading .env.test file")
+	}
+
+	code := m.Run()
+
+	os.Exit(code)
+}
+
+func setupTestData(db *gorm.DB) {
+	items := []models.Item{
+		{Name: "テストアイテム1", Price: 1000, Description: "", SoldOut: false, UserId: 1},
+		{Name: "テストアイテム2", Price: 2000, Description: "テスト2", SoldOut: true, UserId: 1},
+		{Name: "テストアイテム3", Price: 3000, Description: "テスト3", SoldOut: false, UserId: 2},
+	}
+
+	users := []models.User{
+		{Email: "test1@example.com", Password: "test1pass"},
+		{Email: "test2@example.com", Password: "test2pass"},
+	}
+
+	for _, user := range users {
+		db.Create(&user)
+	}
+	for _, item := range items {
+		db.Create(&item)
+	}
+}
+
+func setup() *gin.Engine {
+	db := infra.SetupDB()
+	db.AutoMigrate(&models.Item{}, &models.User{})
+
+	setupTestData(db)
+	router := setupRouter(db)
+
+	return router
+}
+
+func TestFindAll(t *testing.T) {
+	router := setup()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/items", nil)
+
+	// APIリクエストの実行
+	router.ServeHTTP(w, req)
+
+	// APIの実行結果を取得
+	var res map[string][]models.Item
+	json.Unmarshal([]byte(w.Body.String()), &res)
+
+	// アサーション
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, 3, len(res["data"]))
+}
